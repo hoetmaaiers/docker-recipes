aebe53321e56c06fb599a45831e8835aa2dd6c65
hoetmaaiers <houdmeyers@gmail.com>
2017-04-14 17:19:35 +0200

    Add node entrypoint

---
+++ README.md
@@ -0,0 +1,5 @@
+# Docker recipes
+
+## Entrypoints
+
+Docker entrypoints allow for a powerfull grouping of commands to run on your service.
---
+++ entrypoints/node-entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Example of an entrypoint for Node.js apps
+#
+
+# Exit immediately if a command exits with a non-zero status.
+# http://stackoverflow.com/questions/19622198/what-does-set-e-mean-in-a-bash-script
+set -e
+
+# Define help message
+show_help() {
+  echo """
+  Usage: docker run <imagename> COMMAND
+
+  Commands
+  --------
+  bash      : Start a bash shell
+  sails     : Run sails binary 
+  sequelize : Run sails binary 
+  serve     : Serve app through nodemon
+  help      : Show this message
+  """
+}
+
+# Run
+case "$1" in
+  bash)
+    /bin/bash "${@:2}"
+    ;;
+  serve)
+    node_modules/.bin/nodemon "${@:2}"
+    ;;
+  sequelize)
+    node_modules/.bin/sequelize "${@:2}"
+    ;;
+  *)
+    show_help
+    ;;
+esac
+}
